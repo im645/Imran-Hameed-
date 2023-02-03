43729987bbeda0544b66d3b3586a3da20a11de08
im645 <124341029+im645@users.noreply.github.com>
2023-02-03 12:25:54 +0500

    Add files via upload

---
+++ imran hameed.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,mul,sum,sub,div;
+	string name,rollno;
+	name="imran hameed";//i am a student of univerdty of education
+	rollno="bsf2005760";//this is our roll no
+	cout<<"name="<<name;
+	cout<<"\nrollno="<<rollno;
+	a=10; b=5;
+	mul=a*b;
+	 sum=a+b;
+	 sub=a-b;
+	 div=a/b;
+	cout<<"\nmul="<<mul; // This is the value of mul
+	cout<<"\nsum"<<sum; // This is the value of sum
+	cout<<"\nsub"<<sub; // This is the value of sub
+	cout<<"\ndiv"<<div;//Thia is the vakue of div
+
+	return 0;
+}
